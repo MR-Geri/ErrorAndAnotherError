1243a56fcc846ff1dd98fa78718cc06a39db8938
ilyak <ilya.kamckine@ya.ru>
2020-12-11 11:51:29 +0300

    Камера

--- Code/window.py
+++ Code/window.py
@@ -93,4 +93,5 @@ class GameWindow(Window):
                 self.camera_right = False
             if en.type == pg.KEYUP and en.key == pg.K_a:
                 self.camera_left = False
+            #
             self.camera.move(self.camera_left, self.camera_right, self.camera_up, self.camera_down)
