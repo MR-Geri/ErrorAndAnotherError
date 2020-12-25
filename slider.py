09dbdc06eb17f1b24566f5b0811e57791b468201
MR-Geri <ilya.kamckine@ya.ru>
2020-12-25 20:39:24 +0300

    Slider работает хорошо. Добавлен класс для числовых sliders

--- slider.py
+++ slider.py
@@ -4,44 +4,77 @@ import pygame as pg
 from Code.settings import *
 
 
-def volume():
-    pass
+class Numbers:
+    def __init__(self, min_value: int, max_value: int, change: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+        self.change = change
+        self.value = int((max_value - min_value) / 2)
+
+    def edit(self, sign) -> None:
+        self.value = min(self.max_value, max(self.min_value, self.value + sign * self.change))
+
+    def set_value(self, value) -> None:
+        self.value = min(self.max_value, max(self.min_value, value))
 
 
 class Slider:
     def __init__(self, pos: Tuple[int, int], width: int, height: int,
                  color_no_use: COLOR, color_use: COLOR, color_circle: COLOR, func) -> None:
         self.rect = pg.Rect(*pos, width, height)
+        self.circle = pg.Rect(*pos, self.rect.height, self.rect.height)
         self.func = func
         self.color_no_use = color_no_use
         self.color_use = color_use
         self.color_circle = color_circle
-        self.flag = False
+        #
+        self.radius_main, self.rect_height = int(self.rect.height / 2), int(self.rect.height / 2.5)
+        self.radius_mini = int(self.rect_height / 2)
+        self.two_radius = self.radius_main + self.radius_mini
+        self.rect_width = int(self.rect.width - 2 * self.radius_main - 2 * self.radius_mini)
+        self.padding = int((self.rect.height - self.rect_height) / 2)
+        self.pixel_size = self.rect_width + 2 * self.radius_mini
+        self.pixel_change = self.pixel_size / self.func.max_value * self.func.change
+        #
+        self.flag_click = False
         self.render()
 
     def update(self, event: pg.event.Event) -> None:
-        # Если мы зажали кружок -> линейно изменяем
-        # Если мы два раза тыкнули по линии -> Перемещаем кружок туда (меняем значение)
         try:
             if self.rect.collidepoint(*event.pos):
                 if event.type == pg.MOUSEBUTTONUP and event.button == 1:
-                    self.flag = False
-                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and not self.flag:
-                    self.flag = True
+                    self.flag_click = False
+                if self.flag_click:
+                    value = int((event.pos[0] - self.rect.x - self.radius_main) / self.pixel_change * self.func.change)
+                    self.func.set_value(value)
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and not self.flag_click:
+                    self.flag_click = True
+            else:
+                self.flag_click = False
             self.render()
         except AttributeError:
             pass
 
     def render(self) -> None:
-        self.surface = pg.Surface((self.rect.width, self.rect.height))
-        radius_main = self.rect.height // 2
-        height = self.rect.height // 2
-        mini_radius = height // 2
-        width = self.rect.width - height
-        pos = (mini_radius, (self.rect.height - height) // 2)
-        pg.draw.circle(self.surface, self.color_no_use, (pos[0], pos[1] + mini_radius), mini_radius)
-        pg.draw.circle(self.surface, self.color_no_use, (width + mini_radius, pos[1] + mini_radius), mini_radius)
-        pg.draw.rect(self.surface, self.color_no_use, (pos[0], pos[1], width, height))
+        self.surface = pg.Surface((self.rect.width, self.rect.height), pg.SRCALPHA)
+        pg.draw.circle(
+            self.surface, self.color_use, (self.two_radius, self.padding + self.radius_mini), self.radius_mini
+        )
+        pg.draw.circle(
+            self.surface, self.color_no_use,
+            (self.rect.width - self.two_radius, self.padding + self.radius_mini), self.radius_mini
+        )
+        pg.draw.rect(
+            self.surface, self.color_no_use, (self.two_radius, self.padding, self.rect_width, self.rect_height)
+        )
+        w_value = self.pixel_size * (self.func.value / self.func.max_value)
+        pg.draw.rect(self.surface, self.color_use, (self.two_radius, self.padding, w_value, self.rect_height))
+        self.circle.x = self.rect.x + w_value
+        self.circle.y = self.rect.y + self.padding + self.radius_mini - self.radius_main
+        pg.draw.circle(
+            self.surface, self.color_circle,
+            (self.radius_main + w_value, self.padding + self.radius_mini), self.radius_main
+        )
 
 
 class Sliders:
